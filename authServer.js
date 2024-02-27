5b5faef604c8f80fa698d7f0a3766ae5271001a7
ElCapitan <dee.jay.bhx@gmail.com>
2024-02-27 14:55:48 +0000

    completed ready for frontend prototype testing

--- authServer.js
+++ authServer.js
@@ -2,11 +2,13 @@ require("dotenv").config()
 
 const express = require('express');
 const app = express();
-const bcrypt = require("bcrypt")
-const mysql = require("mysql")
-const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt");
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 const DB_HOST = process.env.DB_HOST
 const DB_USER = process.env.DB_USER
@@ -23,9 +25,13 @@ const db = mysql.createPool({
    port: DB_PORT
 })
 
-app.post("/register", async (req,res) => {
-    const user = req.body.username;
-    const hashedPassword = await bcrypt.hash(req.body.password,10);
+    app.get("/", (req, res) => {
+        res.json( "Auth Server/AuthAPI Test");
+    })
+
+    app.post("/register", async (req,res) => {
+        const user = req.body.username;
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
 
     db.getConnection( async (err, connection) => {
      if (err) throw (err)
@@ -55,7 +61,7 @@ app.post("/register", async (req,res) => {
        }
       })
       })
-      }) 
+    }) 
 
 
     function generateAccessToken(user) {
@@ -72,57 +78,80 @@ app.post("/register", async (req,res) => {
         return refreshToken
     }
 
-app.post("/login", async (req,res) => {
-    const user = req.body.username
-    const password = req.body.password
+    app.post("/login", async (req,res) => {
+        const user = req.body.username
+        const password = req.body.password
 
-    db.getConnection ( async (err, connection)=> {
-    if (err) throw (err)
-    const sqlSearch = "Select * from userTable where user = ?"
-    const search_query = mysql.format(sqlSearch,[user])
-    await connection.query (search_query, async (err, result) => {
-    connection.release()
-    
-    if (err) throw (err)
-    if (result.length == 0) {
-    console.log("--------> User does not exist")
-    res.sendStatus(404)
-    } 
-    else {
-        const hashedPassword = result[0].password
-
-        if (await bcrypt.compare(password, hashedPassword)) {
-
-            console.log("---------> Login Successful")
-            console.log("---------> Generating accessToken")
-
-            const accessToken = generateAccessToken ({user: user})
-            const refreshToken = generateRefreshToken ({user: user})
-            res.json ({accessToken: accessToken, refreshToken: refreshToken})
+        db.getConnection ( async (err, connection)=> {
+        if (err) throw (err)
+        const sqlSearch = "Select * from userTable where user = ?"
+        const search_query = mysql.format(sqlSearch,[user])
+        await connection.query (search_query, async (err, result) => {
+        connection.release()
+        
+        if (err) throw (err)
+        if (result.length == 0) {
+        console.log("--------> User does not exist")
+        res.sendStatus(404)
         } 
         else {
-            console.log("--------> Passowrd Incorrect")
-            res.status(401).send("Password Incorrect!")
+            const hashedPassword = result[0].password
+
+            if (await bcrypt.compare(password, hashedPassword)) {
+
+                console.log("---------> Login Successful")
+                console.log("---------> Generating accessToken")
+
+                const accessToken = generateAccessToken ({user: user})
+                const refreshToken = generateRefreshToken ({user: user})
+                res.json ({accessToken: accessToken, refreshToken: refreshToken})
+            } 
+            else {
+                console.log("--------> Passowrd Incorrect")
+                res.status(401).send("Password Incorrect!")
+            }
         }
-    }
-   })
- })
-})
-
-app.post("/refreshToken", (req,res) => {
-    if (!refreshTokens.includes(req.body.token)) res.status(400).send("Refresh Token Invalid")
-    refreshTokens = refreshTokens.filter( (c) => c != req.body.token)
-
-    const accessToken = generateAccessToken ({user: req.body.username})
-    const refreshToken = generateRefreshToken ({user: req.body.username})
-
-    res.json ({accessToken: accessToken, refreshToken: refreshToken})
+    })
+    })
     })
 
-app.delete("/logout", (req,res)=>{
-    refreshTokens = refreshTokens.filter( (c) => c != req.body.token)
+    app.post("/refreshToken", (req,res) => {
+        if (!refreshTokens.includes(req.body.token)) res.status(400).send("Refresh Token Invalid")
+            refreshTokens = refreshTokens.filter( (c) => c != req.body.token)
 
-    res.status(204).send("Logged out!")
+        const accessToken = generateAccessToken ({user: req.body.username})
+        const refreshToken = generateRefreshToken ({user: req.body.username})
+
+        res.json ({accessToken: accessToken, refreshToken: refreshToken})
+    })
+
+    app.delete("/logout", (req,res)=>{
+        refreshTokens = refreshTokens.filter( (c) => c != req.body.token)
+
+        res.status(204).send("Logged out!")
+    })
+
+    function validateToken(req, res, next) {
+        
+        const authHeader = req.headers["authorization"]
+        const token = authHeader.split(" ")[1]
+        
+        if (token == null) res.sendStatus(400).send("Token not present")
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) { 
+         res.status(403).send("Token invalid")
+         }
+         else {
+         req.user = user
+         next() 
+         }
+        }) 
+    } 
+    //this endpoint reresents api call and will be modded and duplicated to authenticate calls to db
+    app.get("/posts", validateToken, (req, res)=>{
+        console.log("Token is valid")
+        console.log(req.user.user)
+        res.send(`${req.user.user} successfully accessed post`)
     })
 
     const port = process.env.TOKEN_SERVER_PORT;
