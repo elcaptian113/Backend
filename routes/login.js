06a5cc205ab313b836337b6e5b926fe3f72959f8
ElCapitan <dee.jay.bhx@gmail.com>
2024-04-26 11:57:13 +0100

    routing added

--- routes/login.js
+++ routes/login.js
@@ -5,3 +5,12 @@ var router = express.Router();
 router.post('/', controller.loginHandler);
 
 module.exports = router;
+const controller = require('../controllers/login');
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/', controller.loginHandler);
+
+
+module.exports = router;
