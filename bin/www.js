bfe5b862e60802fb7275864e59b25f53c76d2745
Danny <lorddarcness@hotmail.com>
2024-05-05 10:41:30 +0100

    in production changes

--- bin/www.js
+++ bin/www.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+//require("dotenv").config()
 
 //declare core varibles and dependencies
 const app = require('../app');
--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+//require("dotenv").config()
 
 //set DB credentials
 module.exports = {
